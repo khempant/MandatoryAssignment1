abff8f244e1db23f6630155ad2b39873fdfbbc84
Khem Pant <khem0026@edu.easj.dk>
2019-10-15 22:02:35 +0200

    Add project files.

---
+++ Book.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace MandatoryAssignment1
+{
+    public class Book
+    {
+        string title;
+        string author;
+        int pageNumber;
+        string isbn13;
+
+        public string Ttle { get => title; }
+        public string Author { get => author; }
+        public int PageNumber { get => pageNumber; }
+        public string Isbn13 { get => Isbn13; }
+
+        public Book(string title, string author, int pageNumber, string isbn13)
+        {
+            Validate(title, author, pageNumber, isbn13);
+        }
+        public Book()
+        {
+
+        }
+        public void Validate(string title, string author, int pageNumber, string isbn13)
+        {
+            if (title.Length > 2)
+                this.title = title;
+            else
+                throw new ArgumentException("Title should be at least 2 character long.");
+            if (pageNumber > 10 && pageNumber <= 1000)
+                this.pageNumber = pageNumber;
+            else
+                throw new ArgumentException("PageNumber should between 10 and 1000.");
+            if (isbn13.Length == 13)
+                this.isbn13 = isbn13;
+            else
+                throw new ArgumentException("isbn must be 13 character long.");
+                 this.author = author;
+           
+        }
+    }
+}
---
+++ Bookcalss.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MandatoryAssignment1
+{
+    public class Bookcalss
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is Book Library");
+            //  List<Book> books = new List<Book>()
+            //  {
+
+            //};
+            Book obj = new Book("Social", "newAuthor",34,"0123456789123");
+             new Book("Science", "newAut", 94, "7123456789123");
+             new Book("Maths", "OldAuthor", 64, "0123456789876");
+
+            Console.ReadLine();
+        }
+         
+
+    }
+}
